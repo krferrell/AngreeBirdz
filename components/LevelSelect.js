c6ade06aff9fe4b83b3d639d4b795a8b25cc84d7
Kenneth Ferrell <kferrell@alphaworks.tech>
2021-10-20 09:58:29 -0700

    feat(levelselect-signin): changed divs to views and added test text

--- components/LevelSelect.js
+++ components/LevelSelect.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Text, View } from 'react-native';
 
 const LevelSelect = () => {
     return (
-        <div>
-            
-        </div>
+        <View>
+            <Text>Level Select</Text>
+        </View>
     )
 }
 
