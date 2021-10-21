2e7cf7fa1472bb8c054c44cce55825de8194c424
Kenneth Ferrell <kferrell@alphaworks.tech>
2021-10-21 16:29:09 -0700

    feat(monkey-game): create grabber monkeys component

---
+++ components/kennyGame/GrabberMonkey.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, View, Image } from "react-native";
+
+export default GrabberMonkey = ({ position }) => {
+  return (
+    <View
+    style={styles({ x: position[0], y: position[1] }).bird}
+    >
+      <Image
+        source={require("../../assets/monkeyGame/monkeyHangingDown.png")}
+        style={{ flex: 1, width: 200, resizeMode: "contain" }}
+      />
+    </View>
+  );
+};
+
+const styles = (props) =>
+  StyleSheet.create({
+    bird: {
+      height: 200,
+      width: 100,
+      borderRadius: 50,
+      // backgroundColor: "red",
+      top: props.x,
+      left: props.y,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  });
