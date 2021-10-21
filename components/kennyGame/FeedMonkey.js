dd88e5181652c340f06c429952a7850bda975d53
Kenneth Ferrell <kferrell@alphaworks.tech>
2021-10-21 16:28:46 -0700

    feat(monkey-game): create feed monkey component

---
+++ components/kennyGame/FeedMonkey.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StyleSheet, View, Image } from "react-native";
+
+export default FeedMonkey = () => {
+  return (
+    <View
+    style={styles.feedMonkey}
+    >
+      <Image
+        source={require("../../assets/monkeyGame/feedMonkey.png")}
+        style={{ flex: 1, width: 200, resizeMode: "contain" }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    feedMonkey: {
+      height: 200,
+      width: 0,
+      borderRadius: 50,
+    //   backgroundColor: "red",
+      position: "absolute",
+      top: 150,
+      right: 0,
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  });
